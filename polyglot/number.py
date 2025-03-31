9ae775b9dd4f528d1661756f5f6e0550be893cba
tails <169904231+kanata-05@users.noreply.github.com>
2025-03-31 23:49:33 +0900

    Main polyglot, I didn't bother with making the filename work, (check extended description)
    This program prints 30 in Whitespace, 29 in Trigger, 28 in Brain-Flak, 27 in Perl 6, 26 in 05AB1E, 25 in Pip, 24 in Thutu, 23 in Hexagony, 22 in Underload, 21 in Nim, 20 in Prelude, 19 in Reng (tested here), 18 in Cardinal, 17 in Julia, 16 in Pyth, 15 in Haystack, 14 in Turtlèd, 13 in Ruby, 12 in Fission, 11 in Befunge-98, 10 in Befunge-93, 9 in Perl 5, 8 in Retina, 7 in Japt, 6 in SMBF, 5 in Python 2, 4 in ><>, 3 in Minkolang, 2 in V/Vim, and 1 in Python 3.
    
    [Space] are spaces, [LF] are linefeed (or newline characters), [Tab] are tabs. This was specified due to Whitespace literally working on this principle. Make sure to change them to what they are before testing.
    
    I didn't bother with making the filename work because it was just too long and just hard, but this is one I came up with,
    
    number.ws.trg.bfl.p6.05ab1e.pip.thu.hxg.und.nim.prl.rng.crd.jl.pyth.hay.turt.rb.fis.b98.b93.pl.ret.japt.smbf.py2.fish.min.vim.py3.py.js

--- polyglot/number.py
+++ polyglot/number.py
@@ -3,5 +3,5 @@
 #=~nJ<R"12";[Space][Tab][LF]
 #[[Tab][LF]
 #`<`|[LF]
-print((eval("1\x2f2")and[Space](9)or(13))-(0and[Space]4)^(1)<<(65)>>62)or'(\{(\{})(\{\/+23!@}[()])}\{})(\{}\{})'#46(8+9+9+9+9+=!)=#print(17)#]#echo[Space]21#|/=1/24=x=90/[LF
+print((eval("1\x2f2")and[Space](9)or(13))-(0and[Space]4)^(1)<<(65)>>62)or'(\{(\{})(\{\/+23!@}[()])}\{})(\{}\{})'#46(8+9+9+9+9+=!)=#print(17)#]#echo[Space]21#|/=1/24=x=90/[LF]
 #8␛dggi2␛[Tab]`[Space]|1|6$//''25[Space][Space]#>say[Tab][Tab][Space]27#T222999"26[LF]
